e9294877b205d98a9739549edf1c24ab30229532
Chris Eastwood <eastwooc2@gmail.com>
2019-10-03 17:54:07 -0700

    implemented setup and teardown

--- test1.py
+++ test1.py
@@ -4,24 +4,22 @@ import unittest
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 
-
 class ParksideExerciseTestCase(unittest.TestCase):
+    # Starting with Wikipedia's https://en.wikipedia.org/wiki/Metis_(mythology) page, please test for the following:
 
-    # def setUp(self):
-    #     """Call before every test case."""
-    #     driver = webdriver.Chrome()
-    #     driver.get('https://en.wikipedia.org/wiki/Metis_(mythology)')
-    #     time.sleep(1)  # Let me actually see something!
-    #     return driver
-    #
-    # def tearDown(self):
-    #     """Call after every test case."""
-    #     driver.quit()
-    #
-    def testATOCsareHeadersOnPage(self):
-        driver = webdriver.Chrome()
-        driver.get('https://en.wikipedia.org/wiki/Metis_(mythology)')
+    def setUp(self):
+        """Call before every test case."""
+        self.driver = webdriver.Chrome()
+        self.driver.get('https://en.wikipedia.org/wiki/Metis_(mythology)')
         time.sleep(1)  # Let me actually see something!
+
+    def tearDown(self):
+        """Call after every test case."""
+        self.driver.quit()
+
+    def testATOCsareHeadersOnPage(self):
+        # the headings listed in the `Contents` box are used as headings on the page
+        driver = self.driver
         toc_titles = driver.find_elements_by_xpath('//*[@id="toc"]/ul/li[*]/a/span[2]')
         headings = driver.find_elements_by_tag_name('H2')
         tocs = len(toc_titles)
@@ -31,12 +29,10 @@ class ParksideExerciseTestCase(unittest.TestCase):
                 if title.text in value.text:
                     actual_h2s = actual_h2s + 1
         assert tocs == actual_h2s
-        driver.quit()
 
     def testBTOCsareHyperLinke(self):
-        driver = webdriver.Chrome()
-        driver.get('https://en.wikipedia.org/wiki/Metis_(mythology)')
-        time.sleep(1) # Let me actually see something!
+        # b) the headings listed in the `Contents` box have functioning hyperlinks
+        driver = self.driver
         toc_links = driver.find_elements_by_xpath('//*[@id="toc"]/ul/li[*]/a')
         for link in toc_links:
             att = link.get_attribute('href')
@@ -47,12 +43,10 @@ class ParksideExerciseTestCase(unittest.TestCase):
             driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.HOME)
             time.sleep(1)
             att = 'null'
-        driver.quit()
 
     def testCPersonifiedNikeText(self):
-        driver = webdriver.Chrome()
-        driver.get('https://en.wikipedia.org/wiki/Metis_(mythology)')
-        time.sleep(1) # Let me actually see something!
+        # c) in the _Personified concepts_, `Nike` has a popup that contains the following text:
+        driver = self.driver
         pc_links = driver.find_elements_by_xpath('//*[@id="mw-content-text"]/div/table[1]/tbody/tr[6]/td/div/ul/li[*]/a')
         for link in pc_links:
             hover = ActionChains(driver).move_to_element(link)
@@ -63,17 +57,15 @@ class ParksideExerciseTestCase(unittest.TestCase):
                 if item.text in 'In ancient Greek religion, Nike was a goddess who personified victory. Her Roman equivalent was Victoria.':
                     tpass = True
         assert tpass == True
-        driver.quit()
 
     def testDNikeFamilyTree(self):
-          driver = webdriver.Chrome()
-          driver.get('https://en.wikipedia.org/wiki/Metis_(mythology)')
-          time.sleep(1)  # Let me actually see something!
+          # d) in the _Personified concepts_, if you click on `Nike`, it takes you to a page that displays a family tree
+          driver = self.driver
           nike = driver.find_elements_by_xpath('//*[@id="mw-content-text"]/div/table[1]/tbody/tr[6]/td/div/ul/li[13]/a')
           for link in nike:
             link.click()
           familytree = driver.find_element_by_xpath('//*[@id="Family_tree"]')
           assert familytree is not 'null'
-          driver.quit()
+
 if __name__ == "__main__":
     unittest.main() # run all tests
